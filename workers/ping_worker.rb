47cd39a725d9f5e9f133b4f564bea07bd3688cf1
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-11-08 00:10:07 -0600

    Upcase env vars

--- workers/ping_worker.rb
+++ workers/ping_worker.rb
@@ -2,7 +2,7 @@ class PingWorker
   include Sidekiq::Worker
   def perform(message)
     adapter = Bazooka::Adapter.fetch('gobierno-federal')
-    adapter.auth(username: ENV['test_user'],  password: ENV['test_password'])
+    adapter.auth(username: ENV['USER'],  password: ENV['PASS'])
     adapter.publish(message)
   end
 end
