d34af58cf0452f4bb935512f39af65156faecd14
rob <rob@partidosurrealista.com>
2015-11-08 00:26:01 -0600

    No sé hacer git, obviamente

--- spec/infomex_spec.rb
+++ spec/infomex_spec.rb
@@ -8,7 +8,7 @@ describe "Infomex" do
   it "should post an information request" do
     infomex = Bazooka::Adapter.fetch('gobierno-federal')
     infomex.auth(username: 'infobazooka',  password: 'iXR/eRVCRK6pwXAePQ4VZ7(%')
-    infomex.publish({
+    folio = infomex.publish({
       user: {
         name: 'Test',
         last_name: ['apellido', 'apellido']
@@ -17,11 +17,13 @@ describe "Infomex" do
         type: 'public',
         dependency: '16',
         text: <<-TEXT
-    En atención al principio de máxima publicidad solicito la base o bases de datos que alimenta(n) la plataforma:http://tramites.semarnat.gob.mx/index.php/consulta-tu-tramite
-     Aludiendo a los principios de gobierno abierto, si estas bases de datos estuviesen en formato de propietario o cerrado, solicito que se me haga entrega de las mismas en algún formato abierto o libre, por ejemplo: csv, json, xml, sql Los archivos deberán ser entregados por medios electrónicos y contener por lo menos la información presentada en http://tramites.semarnat.gob.mx/index.php/consulta-tu-tramite/ incluyendo los Identificadores únicos que utilicen los sistemas para identificar y relacionar los datos. Solicito se me incluya también cualquier tabla o campo adicional no visibles en la  pagina http://tramites.semarnat.gob.mx/index.php/consulta-tu-tramite, que formen parte de la base o el conjunto de bases de datos que utilice el sitio.
-    En caso de contener datos personales se deberá entregar versión pública de la base o bases de datos solicitadas. Gracias
+En atención al principio de máxima publicidad solicito la base o bases de datos que alimenta(n) la plataforma:http://tramites.semarnat.gob.mx/index.php/consulta-tu-tramite
+ Aludiendo a los principios de gobierno abierto, si estas bases de datos estuviesen en formato de propietario o cerrado, solicito que se me haga entrega de las mismas en algún formato abierto o libre, por ejemplo: csv, json, xml, sql Los archivos deberán ser entregados por medios electrónicos y contener por lo menos la información presentada en http://tramites.semarnat.gob.mx/index.php/consulta-tu-tramite/ incluyendo los Identificadores únicos que utilicen los sistemas para identificar y relacionar los datos. Solicito se me incluya también cualquier tabla o campo adicional no visibles en la  pagina http://tramites.semarnat.gob.mx/index.php/consulta-tu-tramite, que formen parte de la base o el conjunto de bases de datos que utilice el sitio.
+  En caso de contener datos personales se deberá entregar versión pública de la base o bases de datos solicitadas. Gracias
     TEXT
       }
     })
+
+    puts folio
   end
 end
