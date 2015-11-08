30a75e5a3ba42226a16fe7fcd3ccf01d39147df8
rob <rob@partidosurrealista.com>
2015-11-08 00:39:09 -0600

    cambiando expiration de sidekiq

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ configure do
 
   Sidekiq.configure_server do |config|
     config.server_middleware do |chain|
-      chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
+      chain.add Sidekiq::Status::ServerMiddleware, expiration: 30*60 # default
     end
     config.client_middleware do |chain|
       chain.add Sidekiq::Status::ClientMiddleware
